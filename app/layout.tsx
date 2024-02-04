6ca1197e1aad2c9febb513cc2c17e2ac4847ef4a
Guilherme Barros <g.henrique.barros21@gmail.com>
2024-02-04 17:36:18 -0300

    arrumando o footer no a aba agendamentos

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} dark`}>
       <AuthProvider>
-        {children}
+
+          <div className="flex-1">
+            {children}
+          </div>
+
         <Toaster/>
         <Footer />
       </AuthProvider>
