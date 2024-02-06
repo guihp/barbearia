75fedb3f3cba66a35ff096a25960039db0e1eead
Guilherme Barros <g.henrique.barros21@gmail.com>
2024-02-05 22:28:37 -0300

    mini alteração

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return ( 
         <footer className="w-full bg-secondary py-6 px-5">
             <p className="text-gray-400 text-xs font-bold opacity-75">
-                © 2023 Copyright FSW Barber
+                © 2023 Copyright styler Barber
             </p>
         </footer>
      );
